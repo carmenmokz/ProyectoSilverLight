cfef3b479b541ae823b2831a185409a3a773f884
Carmen <carmenmok1697@gmail.com>
2016-11-18 14:05:07 -0600

    Creacion Clases 2

---
+++ ProyectoSilverLight.Web/clCliente.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.ObjectModel;
+
+namespace ProyectoSilverLight.Web
+{
+    [DataContract]
+    public class clCliente
+    {
+        [DataMember]
+        public String IDCLIENTE { get; set; }
+        [DataMember]
+        public String NOMBRE { get; set; }
+        [DataMember]
+        public String APELLIDO1 { get; set; } 
+        [DataMember]
+        public String APELLIDO2 { get; set; }
+        [DataMember]
+        public String CORREO { get; set; }
+        [DataMember]
+        public String IDCLIENTE1 { get; set; } 
+        [DataMember]
+        public int NUMFACTURA { get; set; }
+        [DataMember]
+        public int CODDIRECCION { get; set; }
+        [DataMember]
+        public int CODCOMPRA { get; set; }
+
+        public clCliente(String idcliente, String nombre, String apellido1, String apellido2, String correo, String idcliente1,
+                        int numfactura, int coddireccion, int codcompra)
+        {
+            this.IDCLIENTE = idcliente;
+            this.NOMBRE = nombre;
+            this.APELLIDO1 = apellido1;
+            this.APELLIDO2 = apellido2;
+            this.CORREO = correo;
+            this.IDCLIENTE1 = idcliente;
+            this.NUMFACTURA = numfactura;
+            this.CODDIRECCION = coddireccion;
+            this.CODCOMPRA = codcompra;
+
+        }
+    }
+}
---
+++ ProyectoSilverLight.Web/clConfiguracion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Collections.ObjectModel;
+
+namespace ProyectoSilverLight.Web
+{
+    [DataContract]
+    public class clConfiguracion
+    {
+        [DataMember]
+        public int CODIGOVALORESPERMITIDOS { get; set; }
+        [DataMember]
+        public int CODIGOLINEA { get; set; }
+        [DataMember]
+        public String CONFIGURACION { get; set; }
+        [DataMember]
+        public int CODIGOATRIBUTO { get; set; }
+        [DataMember]
+        public String DESCRIPCION { get; set; }
+        [DataMember]
+        public int CODIGOPRODUCTO { get; set; }
+        [DataMember] 
+        public int NUMFACTURA { get; set; } 
+);
+
+        public clConfiguracion(int codvalorespermitidos, int codlinea, String configuracion, int codatributo, String descripcion,
+                            int codproducto, int numfactura)
+        {
+            this.CODIGOVALORESPERMITIDOS = codvalorespermitidos;
+            this.CODIGOLINEA = codlinea;
+            this.CONFIGURACION = configuracion;
+            this.CODIGOATRIBUTO = codatributo;
+            this.DESCRIPCION = descripcion;
+            this.CODIGOPRODUCTO = codproducto;
+            this.NUMFACTURA = numfactura;
+
+        }
+    }
+}
